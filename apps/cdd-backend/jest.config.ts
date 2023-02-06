910cbb38910386aec4457c32e70d883153d2fb59
Eric <ericrichardson@polymath.network>
2023-02-06 09:38:51 -0500

    Refactor from "packages" to "apps" and "libs" directories

--- apps/cdd-backend/jest.config.ts
+++ apps/cdd-backend/jest.config.ts
@@ -12,5 +12,5 @@ export default {
     '^.+\\.[tj]s$': 'ts-jest',
   },
   moduleFileExtensions: ['ts', 'js', 'html'],
-  coverageDirectory: '../../coverage/packages/cdd-backend',
+  coverageDirectory: '../../coverage/apps/cdd-backend',
 };
