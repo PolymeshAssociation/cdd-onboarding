c8b1f05d9c4ea9907dea483add9cc68cb76134dd
Toms Veidemanis <toms@veidemanis.lv>
2023-03-06 17:17:05 +0200

    deps: add react-query

--- apps/cdd-frontend/src/main.tsx
+++ apps/cdd-frontend/src/main.tsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react';
 import * as ReactDOM from 'react-dom/client';
 import { RouterProvider } from 'react-router-dom';
+import { QueryClient, QueryClientProvider } from 'react-query';
 
 import { PolymeshTheme } from '@polymeshassociation/polymesh-theme';
 
@@ -9,10 +10,15 @@ import { router } from './pages';
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+
+const queryClient = new QueryClient();
+
 root.render(
   <StrictMode>
-    <PolymeshTheme>
-      <RouterProvider router={router} />
-    </PolymeshTheme>
+    <QueryClientProvider client={queryClient}>
+      <PolymeshTheme>
+        <RouterProvider router={router} />
+      </PolymeshTheme>
+    </QueryClientProvider>
   </StrictMode>
 );
