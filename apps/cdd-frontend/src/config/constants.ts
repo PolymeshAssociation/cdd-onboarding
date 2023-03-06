c77bb374453a2c5728bbfd7da0b92164f7013b50
Toms Veidemanis <toms@veidemanis.lv>
2023-03-07 00:22:16 +0200

    feat: add logger service

--- apps/cdd-frontend/src/config/constants.ts
+++ apps/cdd-frontend/src/config/constants.ts
@@ -1,2 +1,4 @@
 // TODO: replace this with brackets for server variables
 export const API_URL = process.env.NODE_ENV === 'development' ? 'http://localhost:3333' : 'https://polymesh.cdd.centrifuge.io/api';
+
+export const LOG_LEVEL =  process.env.NODE_ENV === 'development' ? 'log' : 'error';
