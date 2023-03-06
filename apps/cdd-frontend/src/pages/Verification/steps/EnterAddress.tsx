8d586110f505314259e809d51642abbc4d8e827f
Toms Veidemanis <toms@veidemanis.lv>
2023-03-07 00:23:27 +0200

    feat: implement verify address

--- apps/cdd-frontend/src/pages/Verification/steps/EnterAddress.tsx
+++ apps/cdd-frontend/src/pages/Verification/steps/EnterAddress.tsx
@@ -1,42 +1,51 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect } from 'react';
 import { useForm } from 'react-hook-form';
 import { zodResolver } from '@hookform/resolvers/zod'
 import { z } from "zod";
+import { Box, Input, Text, CircularProgress } from '@chakra-ui/react';
 
-import { FormContext } from '@polymeshassociation/polymesh-theme/ui/organisms';
-import { Box, Input, Text } from '@chakra-ui/react';
+import { FormContext, FormNavigation } from '@polymeshassociation/polymesh-theme/ui/organisms';
 import { addressZ } from "@cdd-onboarding/cdd-types/utils";
-import { API_URL } from '../../../config/constants';
+
+import useVerifyAddressMutation from '../../../hooks/useVerifyAddressMutation'
 
 const schema = z.object({
   address: addressZ
 });
 
+type FormValues = z.infer<typeof schema>;
+
 export const FirstStep: React.FC = () => {
   const {
     register,
     handleSubmit,
-    watch,
     formState: { errors },
-  } = useForm({ resolver: zodResolver(schema) });
+  } = useForm<FormValues>({ resolver: zodResolver(schema) });
   const { onNext } = useContext(FormContext);
+  const { mutate, isLoading, isSuccess } = useVerifyAddressMutation();
 
-  const onSubmit = (data: unknown) => {
-    console.log(data);
-    onNext();
+  const onSubmit = ({ address }: FormValues) => {
+    mutate(address)    
   };
 
-  console.log(errors)
+  useEffect(()=>{
+    if(isSuccess){
+      onNext();
+    }
+  }, [isSuccess])
+
+  console.log(isLoading, isSuccess)
 
   return (
     <form id="stepForm" onSubmit={handleSubmit(onSubmit)}>
       <Box maxW="500px">
         <Text mb="0.5rem" fontSize="sm" >
-          Polymesh Address {API_URL}
+          Polymesh Address
         </Text>
         <Input placeholder="Polymesh Address" size="lg" {...register("address")} />
         {errors.address?.message && <Text>{errors.address?.message.toString()}</Text>}
       </Box>
+      <FormNavigation nextStepLabel="Get started" nextIsLoading={isLoading} nextLoadingLabel={<><CircularProgress size="1.5rem" isIndeterminate color='white' mr="1rem" /> Verifying...</>} />
     </form>
   );
 };
