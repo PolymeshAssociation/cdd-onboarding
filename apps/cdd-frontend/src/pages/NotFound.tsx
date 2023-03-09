f0175a164dd2194db9a2654ba82440ae686ae8d1
Toms Veidemanis <toms@veidemanis.lv>
2023-03-09 15:36:36 +0200

    feat: update not found style

--- apps/cdd-frontend/src/pages/NotFound.tsx
+++ apps/cdd-frontend/src/pages/NotFound.tsx
@@ -1,9 +1,31 @@
-import React from 'react'
+import { Box, Heading, Text, Button } from '@chakra-ui/react';
 
-const NotFound = () => {
+export const NotFound = () => {
   return (
-    <div>404</div>
-  )
+    <Box textAlign="center" py="calc(50% - 350px)" px={6}>
+      <Heading
+        display="inline-block"
+        as="h1"
+        size="4xl"
+        >
+        404
+      </Heading>
+      <Text fontSize="2xl" mt={3} mb={2}>
+        Page Not Found
+      </Text>
+      <Text color={'gray.500'} mb={6}>
+        The page you're looking for does not seem to exist
+      </Text>
+
+      <Button
+        as="a"
+        href="/"
+        colorScheme="navy"
+        variant="ghost">
+        Go to Home
+      </Button>
+    </Box>
+  );
 }
 
-export default NotFound
+export default NotFound;
