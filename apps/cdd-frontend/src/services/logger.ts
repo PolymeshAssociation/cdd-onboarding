2bf4dd7ec3f79d655232ff26bb56576203b4b1f3
Toms Veidemanis <toms@veidemanis.lv>
2023-03-27 22:38:12 +0300

    fix: fe logger refactor

--- apps/cdd-frontend/src/services/logger.ts
+++ apps/cdd-frontend/src/services/logger.ts
@@ -18,47 +18,34 @@ const NO_OP: LogFn = (message?: unknown, ...optionalParams: unknown[]) => {};
 
 /** Logger which outputs to the browser console */
 export class ConsoleLogger implements Logger {
-  readonly log!: LogFn;
-  readonly debug: LogFn;
-  readonly warn: LogFn;
-  readonly error: LogFn;
+  readonly log: LogFn = NO_OP;
+  readonly debug: LogFn = NO_OP;
+  readonly warn: LogFn = NO_OP;
+  readonly error: LogFn = NO_OP;
 
   constructor(options?: { level?: AppConfig['LOG_LEVEL'] }) {
     const { level } = options || {};
 
     if (level === 'off') {
-      this.warn = NO_OP;
-      this.log = NO_OP;
-      this.debug = NO_OP;
-      this.error = NO_OP;
-
       return;
     }
 
     this.error = console.error.bind(console);
 
     if (level === 'error') {
-      this.warn = NO_OP;
-      this.log = NO_OP;
-      this.debug = NO_OP;
-
       return;
     }
 
+    
     this.warn = console.warn.bind(console);
-
+    
     if (level === 'warn') {
-      this.log = NO_OP;
-      this.debug = NO_OP;
-
       return;
     }
 
     this.warn = console.log.bind(console);
 
     if (level === 'log') {
-      this.debug = NO_OP;
-
       return;
     }
 
