acdc2937cbe2c5800b4b153ca739a63d8ba05db1
Toms Veidemanis <toms@veidemanis.lv>
2023-06-20 15:00:16 +0300

    feat: ui updates (colors, center align, bg image)

--- libs/polymesh-theme/src/lib/ui/organisms/Hero/Hero.tsx
+++ libs/polymesh-theme/src/lib/ui/organisms/Hero/Hero.tsx
@@ -1,4 +1,9 @@
-import { Heading, VStack, Flex, Box, useColorModeValue } from '@chakra-ui/react';
+import {
+  Heading,
+  VStack,
+  Box,
+  useColorModeValue,
+} from '@chakra-ui/react';
 import React from 'react';
 import { SectionContainer } from '../../atoms';
 
@@ -10,8 +15,8 @@ export type HeroProps = {
 };
 export const Hero: React.FC<HeroProps> = ({ title, subtitle, cta, align }) => {
   let justify = 'flex-start';
-  const headingColor = useColorModeValue("text.light", "text.dark")
-  const subheadingColor = useColorModeValue("gray.500", "gray.100")
+  const headingColor = useColorModeValue('heading.light', 'heading.dark');
+  const subheadingColor = useColorModeValue('text.light', 'gray.200');
 
   if (align === 'center') {
     justify = 'center';
@@ -23,17 +28,33 @@ export const Hero: React.FC<HeroProps> = ({ title, subtitle, cta, align }) => {
 
   return (
     <SectionContainer>
-    <Flex justify={justify} py={{ base: '1rem', md: '2rem'}} w="100%">
-      <VStack maxW={{ base: '100%', md: '50%'}} align={justify}>
-        <Heading as="h1" size={{ base: '4xl', sm: '3xl' }} my="0.5rem !important" color={headingColor} lineHeight="1.5em">
-          {title}
-        </Heading>
-        <Heading as="h2" size="lg" color={subheadingColor} my="0.75rem !important" lineHeight="1.5em">
-          {subtitle}
-        </Heading>
-        {Boolean(cta) && <Box mt="2.5rem !important" w="100%">{cta}</Box>}
-      </VStack>
-    </Flex>
+        <VStack align={justify} maxW={{ base: "100%", md: "50%"}}>
+          <Heading
+            as="h1"
+            fontSize="3rem"
+            my="0.67em !important"
+            color={headingColor}
+            fontWeight={600}
+            lineHeight="140%"
+            letterSpacing="-0.03em"
+          >
+            {title}
+          </Heading>
+          <Heading
+            as="h2"
+            fontSize="1.125rem"
+            color={subheadingColor}
+            my="0.75rem !important"
+            lineHeight="1.5em"
+          >
+            {subtitle}
+          </Heading>
+          {Boolean(cta) && (
+            <Box mt="2.5rem !important" w="100%">
+              {cta}
+            </Box>
+          )}
+        </VStack>
     </SectionContainer>
   );
 };
